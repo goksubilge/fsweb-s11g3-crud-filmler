aadd9a15ede6f87c752171142edadc8914cc459e
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-08-10 14:08:14 +0300

    add esnek görev 1

--- src/App.js
+++ src/App.js
@@ -41,7 +41,11 @@ const App = (props) => {
       });
   };
 
-  const addToFavorites = (movie) => {};
+  const addToFavorites = (movieId) => {
+    const favMovie = movies.find((movie) => movie.id === movieId);
+    if (!favoriteMovies.find((movie) => movie.id === movieId))
+      setFavoriteMovies([...favoriteMovies, favMovie]);
+  };
 
   return (
     <div>
@@ -64,7 +68,10 @@ const App = (props) => {
             </Route>
 
             <Route path="/movies/:id">
-              <Movie deleteMovie={deleteMovie} />
+              <Movie
+                deleteMovie={deleteMovie}
+                addToFavorites={addToFavorites}
+              />
             </Route>
 
             <Route path="/movies">
